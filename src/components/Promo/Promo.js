9ef13b431207fcd1b4645888fc667baa2ba21596
Alena Goncharova <alenalesk@gmail.com>
2021-03-16 22:31:36 +0300

    implemented promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,4 +1,5 @@
 import './Promo.css';
+import promoImage from '../../images/landing-logo.svg';
 
 
 
@@ -6,7 +7,10 @@ function Promo() {
 
   return (
     <div className="Promo">
-    
+      <img src={promoImage} className="Promo__image" alt="Земной шар Web" />
+      <h1 className="Promo__header">Учебный проект студента факультета Веб-разработки.</h1>
+      <h4 className="Promo__caption">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</h4>
+      <button type="button" className="Promo__button">Узнать больше</button>
     </div>
   );
 }
