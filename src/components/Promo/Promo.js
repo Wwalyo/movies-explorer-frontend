cf7cebcdf2ad82f018fab5bed584e6671989c46b
Alena <alenalesk@gmail.com>
2021-03-17 17:46:14 +0300

    adapted Header and Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -8,9 +8,11 @@ function Promo() {
   return (
     <div className="Promo">
       <img src={promoImage} className="Promo__image" alt="Земной шар Web" />
-      <h1 className="Promo__header">Учебный проект студента факультета Веб-разработки.</h1>
-      <h4 className="Promo__caption">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</h4>
-      <button type="button" className="Promo__button">Узнать больше</button>
+      <div className="Promo__info">
+        <h1 className="Promo__header">Учебный проект студента факультета Веб-разработки.</h1>
+        <h4 className="Promo__caption">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</h4>
+        <button type="button" className="Promo__button">Узнать больше</button>
+      </div>
     </div>
   );
 }
