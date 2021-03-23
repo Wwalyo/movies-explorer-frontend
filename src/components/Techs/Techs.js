561492980d78a5db6ebbb0a932895d706e39281a
Alena Goncharova <alenalesk@gmail.com>
2021-03-23 23:21:12 +0300

    Techs done

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -9,15 +9,15 @@ function Techs() {
       <h3 className="AboutProject__title">Технологии</h3>
       <hr className="Techs__line"></hr>
       <h2 className="Techs__subtitle">7 технологий</h2>
-      <p className="AboutProject__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-      <div className="Techs-grid">
-        <p className="Techs-grid__item">HTML</p>
-        <p className="Techs-grid__item">CSS</p>
-        <p className="Techs-grid__item">JS</p>
-        <p className="Techs-grid__item">React</p>
-        <p className="Techs-grid__item">Git</p>
-        <p className="Techs-grid__item">Express.js</p>
-        <p className="Techs-grid__item">mongoDB</p>
+      <p className="Techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <div className="Techs-scroll">
+        <p className="Techs-scroll__item">HTML</p>
+        <p className="Techs-scroll__item">CSS</p>
+        <p className="Techs-scroll__item">JS</p>
+        <p className="Techs-scroll__item">React</p>
+        <p className="Techs-scroll__item">Git</p>
+        <p className="Techs-scroll__item">Express.js</p>
+        <p className="Techs-scroll__item">mongoDB</p>
       </div>
     </div>
   );
