1af0cba14ef9127726c7d9c7dfb5af4a70f56ae6
Alena <alenalesk@gmail.com>
2021-08-23 17:39:18 +0300

    revisions

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,13 +7,13 @@ function Navigation() {
       <div className="Navigation__container">
         <button type="reset" className="Navigation__close-button"></button>
         <ul className="Navigation__list">
-          <li>
+          <li className="Navigation__list-item">
             <Link to="/" className="Navigation__item">Главная</Link>
           </li>
-          <li>
+          <li className="Navigation__list-item">
             <Link to="/movies" className="Navigation__item">Фильмы</Link>
           </li>
-          <li>
+          <li className="Navigation__list-item">
             <Link to="/saved-movies" className="Navigation__item">Сохраненные фильмы</Link>
           </li>
         </ul>
